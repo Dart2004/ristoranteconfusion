b057e9953cf6432e1158509c612dd47c23251019
Dart2.0 <supercash1@web.de>
2020-07-05 17:39:18 +0200

    Hey

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,23 +4,38 @@ import DishDetail from "./DishdetailComponent";
 import { DISHES } from '../shared/dishes';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from './HomeComponent';
+import {COMMENTS} from "../shared/comments";
+import {PROMOTIONS} from "../shared/promotions";
+import {LEADERS} from "../shared/leaders";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      selectedDish: null,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     };
   }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId})
-  }
   render(){
+    const HomePage = () => {
+      return(
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]} leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+      )
+    }
     return (
       <div>
         <Header></Header>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}></DishDetail>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer></Footer>
       </div>
     );
